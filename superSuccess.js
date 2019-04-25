1d8dda1fdf5a72f45cf5f8404e900b749a971e58
wilsonchen <wilsonchen@superbank.com>
2019-04-25 17:50:40 +0800

    first build

---
+++ superSuccess.js
@@ -0,0 +1,19 @@
+
+var create_random_string = (random_dom) => {
+    for (i = 0; i <=random_dom; i++) {
+        (function (i) {
+            setTimeout(function () {
+                //console.log(random_dom-i);
+                if(random_dom-i == 0){
+                    document.getElementsByClassName("ssJumpRed")[0].innerHTML = random_dom-i;
+                    console.log("跳轉頁面");
+                }
+                else{
+                    document.getElementsByClassName("ssJumpRed")[0].innerHTML = random_dom-i;
+                }
+            }, (i + 1) * 1000);
+        })(i);
+    }
+}
+
+create_random_string(5);
